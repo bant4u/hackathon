dcd3348ad4927aaff3c8fb2864d033e42f3a609c
krishna <krishna@-.localnet.com>
2014-05-31 22:26:57 +0545

    Rainfall finished

--- ews/views.py
+++ ews/views.py
@@ -1,19 +1,13 @@
-<<<<<<< HEAD
 import requests
 from bs4 import BeautifulSoup
-=======
-import re,datetime
->>>>>>> 345c675a69be4bb18bad8867b2817fe4ca3dbd7c
 from django.shortcuts import render, render_to_response
-import json
-import time
 
 # Create your views here.
 def client_form(request):
-	farmer_notificantion()
+	print request.user.password
+	print request.user.username
 	return render(request,'client.html')
 
-<<<<<<< HEAD
 def home_page(request):
 	return render(request,'/login/')
 
@@ -48,26 +42,8 @@ def rainfall_watch(request):
 
 		count=len(results)
 	# print results
-	return render(request, "client.html",{'results': results},  {"count" : count })
+	return render(request, "client.html",{'results': results})
 
 def google_map(request):
 	return render(request,"googleMap.html")
-=======
-def farmer_notificantion():
-	rice = {'avg_rain' :  80, 'min_humidity':60, 'max_humidity':80, 'min_temp':12, 'max_temp':24 }
-	maize = {'avg_rain' : 80, 'min_humidity':60, 'max_humidity':80, 'min_temp':12, 'max_temp':24 }
-	sugarcane = {'avg_rain' : 80, 'min_humidity':60, 'max_humidity':80, 'min_temp':12, 'max_temp':24 }
-
-	files  =  open('/home/bijay/hackathon/hackathon/ews/sample_rain_data.json')
-	data = json.load(files)
-
-	for i, x in enumerate(data):
-		if i%3:
-			from_date = x['date']
-			conv = time.strptime(from_date, "%y-%m-%r")
-			print time.strftime("")
-			d=datetime.datetime(*map(int, re.split('[^\d]', s)[:-1]))
-
-
->>>>>>> 345c675a69be4bb18bad8867b2817fe4ca3dbd7c
 
