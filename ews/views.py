93a21fced907807db6bc090eefb52b1de1f9b74e
krishna <krishna@-.localnet.com>
2014-05-31 11:41:45 +0545

    Login completed

---
+++ ews/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render, render_to_response
+
+# Create your views here.
+def client_form(request):
+	print request.user.password
+	print request.user.username
+	return render(request,'client.html')
